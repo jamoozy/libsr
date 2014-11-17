efc3696751913df58bcaf16fd7efe7f4f3733c19
Andrew "Jamoozy" Correa S <jamoozy@gmail.com>
2014-11-16 16:42:26 -0800

    Added general paleo header file.

---
+++ src/paleo/paleo.h
@@ -0,0 +1,11 @@
+#ifndef  __paleo_h__
+#define  __paleo_h__
+
+#include "point.h"
+#include "line.h"
+#include "circle.h"
+#include "curve.h"
+#include "helix.h"
+#include "spiral.h"
+
+#endif  //__paleo_h__
