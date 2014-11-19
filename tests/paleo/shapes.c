20b19984e5ec5d128611fde02184b5b99b875748
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-11-18 16:32:17 -0800

    Conformed shape tests to new API.

--- tests/paleo/shapes.c
+++ tests/paleo/shapes.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <check.h>
 
 #include "common/mock_point.h"
@@ -18,32 +19,25 @@ START_TEST(c_line_create)
   line_t* line = line_create();
   ck_assert(line != NULL);
   ck_assert_int_eq(line->num, 2);
-  ck_assert(line->points != NULL);
+  ck_assert(line->pts != NULL);
   line_destroy(line);
 }
 END_TEST
 
 START_TEST(c_line_create_points_with_points)
 {
-  point_t* a = point_create_full( 5, 10, 20, 40);
-  point_t* b = point_create_full(12, 24, 36, 48);
-  line_t* line = line_create_points_with_points(a, b);
+  point_t a = { 5, 10, 20, 40};
+  point_t b = {12, 24, 36, 48};
+  line_t* line = line_create_points_with_points((point2d_t*)&a, (point2d_t*)&b);
 
   ck_assert(line != NULL);
   ck_assert_int_eq(line->num, 2);
-  ck_assert(line->points != NULL);
 
-  ck_assert(line->points[0] == a);
-  ck_assert_int_eq(line->points[0]->x,  5);
-  ck_assert_int_eq(line->points[0]->y, 10);
-  ck_assert_int_eq(line->points[0]->t, 20);
-  ck_assert_int_eq(line->points[0]->i, 40);
-
-  ck_assert(line->points[1] == b);
-  ck_assert_int_eq(line->points[1]->x, 12);
-  ck_assert_int_eq(line->points[1]->y, 24);
-  ck_assert_int_eq(line->points[1]->t, 36);
-  ck_assert_int_eq(line->points[1]->i, 48);
+  ck_assert(line->pts != NULL);
+  ck_assert_int_eq(line->pts[0].x,  5);
+  ck_assert_int_eq(line->pts[0].y, 10);
+  ck_assert_int_eq(line->pts[1].x, 12);
+  ck_assert_int_eq(line->pts[1].y, 24);
 
   line_destroy(line);
 }
@@ -55,15 +49,12 @@ START_TEST(c_line_create_points_with_longs)
 
   ck_assert(line != NULL);
   ck_assert_int_eq(line->num, 2);
-  ck_assert(line->points != NULL);
 
-  ck_assert(line->points[0] != NULL);
-  ck_assert_int_eq(line->points[0]->x, -20);
-  ck_assert_int_eq(line->points[0]->y,  20);
-
-  ck_assert(line->points[1] != NULL);
-  ck_assert_int_eq(line->points[1]->x, -40);
-  ck_assert_int_eq(line->points[1]->y,  40);
+  ck_assert(line->pts != NULL);
+  ck_assert_int_eq(line->pts[0].x, -20);
+  ck_assert_int_eq(line->pts[0].y,  20);
+  ck_assert_int_eq(line->pts[1].x, -40);
+  ck_assert_int_eq(line->pts[1].y,  40);
 
   line_destroy(line);
 }
@@ -78,33 +69,32 @@ END_TEST
 START_TEST(c_circle_create)
 {
   circle_t* circle = circle_create();
-
   ck_assert(circle != NULL);
   ck_assert_int_eq(circle->r, 0);
-  ck_assert(circle->c != NULL);
+  ck_assert_int_eq(circle->c.x, 0);
+  ck_assert_int_eq(circle->c.y, 0);
 }
 END_TEST
 
 START_TEST(c_circle_create_full)
 {
   circle_t* circle = circle_create_full(40, 20, 80);
-
   ck_assert(circle != NULL);
   ck_assert_int_eq(circle->r, 40);
-  ck_assert(circle->c != NULL);
-  ck_assert_int_eq(circle->c->x, 20);
-  ck_assert_int_eq(circle->c->y, 80);
+  ck_assert_int_eq(circle->c.x, 20);
+  ck_assert_int_eq(circle->c.y, 80);
 }
 END_TEST
 
 START_TEST(c_circle_create_with_point)
 {
-  point_t* point = point_create();
-  circle_t* circle = circle_create_with_point(40, point);
+  point_t point = { 4, 8, 32, 49};
+  circle_t* circle = circle_create_with_point(40, (point2d_t*)&point);
 
   ck_assert(circle != NULL);
   ck_assert_int_eq(circle->r, 40);
-  ck_assert(circle->c == point);
+  ck_assert_int_eq(circle->c.x, point.x);
+  ck_assert_int_eq(circle->c.y, point.y);
 }
 END_TEST
 
@@ -119,7 +109,7 @@ START_TEST(c_curve_create)
   curve_t* curve = curve_create(4);
   ck_assert(curve != NULL);
   ck_assert_int_eq(curve->num, 4);
-  ck_assert(curve->points != NULL);
+  ck_assert(curve->pts != NULL);
   curve_destroy(curve);
 }
 END_TEST
@@ -131,11 +121,10 @@ START_TEST(c_curve_create_coords)
   curve_t* curve = curve_create_coords(4, xs, ys);
   ck_assert(curve != NULL);
   ck_assert_int_eq(curve->num, 4);
-  ck_assert(curve->points != NULL);
+  ck_assert(curve->pts != NULL);
   for (int i = 0; i < curve->num; i++) {
-    ck_assert(curve->points[i] != NULL);
-    ck_assert_int_eq(curve->points[i]->x, xs[i]);
-    ck_assert_int_eq(curve->points[i]->y, ys[i]);
+    ck_assert_int_eq(curve->pts[i].x, xs[i]);
+    ck_assert_int_eq(curve->pts[i].y, ys[i]);
   }
   curve_destroy(curve);
 }
@@ -145,21 +134,21 @@ START_TEST(c_curve_create_points)
 {
   long xs[] = { 4, 8, 12, 16, 28 };
   long ys[] = { 12, 24, 48, 62 };
-  point_t* points[] = {
-    point_create_coords(xs[0], ys[0]),
-    point_create_coords(xs[1], ys[1]),
-    point_create_coords(xs[2], ys[2]),
-    point_create_coords(xs[3], ys[3])
+  point2d_t points[] = {
+    { xs[0], ys[0] },
+    { xs[1], ys[1] },
+    { xs[2], ys[2] },
+    { xs[3], ys[3] }
   };
+
   curve_t* curve = curve_create_points(4, points);
+
   ck_assert(curve != NULL);
   ck_assert_int_eq(curve->num, 4);
-  ck_assert(curve->points != NULL);
-  ck_assert(curve->points != points);
+  ck_assert(curve->pts != NULL);
   for (int i = 0; i < curve->num; i++) {
-    ck_assert(curve->points[i] == points[i]);
-    ck_assert_int_eq(curve->points[i]->x, xs[i]);
-    ck_assert_int_eq(curve->points[i]->y, ys[i]);
+    ck_assert(curve->pts[i].x == xs[i]);
+    ck_assert(curve->pts[i].y == ys[i]);
   }
   curve_destroy(curve);
 }
@@ -202,7 +191,7 @@ int main() {
   Suite* suite = shapes_suite();
   SRunner* runner = srunner_create(suite);
 
-  srunner_run_all(runner, CK_NORMAL);
+  srunner_run_all(runner, CK_MINIMAL);
   number_failed = srunner_ntests_failed(runner);
   srunner_free(runner);
 
