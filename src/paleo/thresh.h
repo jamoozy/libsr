9bcf81929cb6d09e91594d9bf7f96245c422e6c9
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-11-19 22:40:07 -0800

    Added Paleo threshold list.

---
+++ src/paleo/thresh.h
@@ -0,0 +1,31 @@
+#ifndef __thresholds_h__
+#define __thresholds_h__
+
+#define PALEO_THRESH_A   0.5
+#define PALEO_THRESH_B   5.0
+#define PALEO_THRESH_C  70.0
+#define PALEO_THRESH_D   1.31
+#define PALEO_THRESH_E   0.16
+#define PALEO_THRESH_F   0.75
+#define PALEO_THRESH_G   2.0
+#define PALEO_THRESH_H  10.25
+#define PALEO_THRESH_I   0.0036
+#define PALEO_THRESH_J   6.0
+#define PALEO_THRESH_K   0.8
+#define PALEO_THRESH_L  30.0
+#define PALEO_THRESH_M   0.33
+#define PALEO_THRESH_N  16.0
+#define PALEO_THRESH_O   0.425
+#define PALEO_THRESH_P   0.35
+#define PALEO_THRESH_Q   0.4
+#define PALEO_THRESH_R   0.37
+#define PALEO_THRESH_S   0.9
+#define PALEO_THRESH_T   0.25
+#define PALEO_THRESH_U   0.2
+#define PALEO_THRESH_V   0.1
+#define PALEO_THRESH_W   9.0
+#define PALEO_THRESH_X  10.0
+#define PALEO_THRESH_Y   0.99
+#define PALEO_THRESH_Z   0.06
+
+#endif  // __thresholds_h__
