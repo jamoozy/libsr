04d5dd07d2d5dc9fff03dfe6052ab450846c10c6
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-11-25 12:55:47 -0800

    Added MIN and MAX macros to common/util.

--- src/common/util.h
+++ src/common/util.h
@@ -7,5 +7,18 @@
 
 #define get_utime() ((long)time(NULL))
 
+// Min and max copied from StackOverflow:
+//   http://stackoverflow.com/questions/3437404/min-and-max-in-c
+
+#define MAX(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define MIN(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 #endif  // __util_h__
 
