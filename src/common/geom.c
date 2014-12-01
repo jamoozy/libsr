918038310cf6ba1df4fb369f87aebb7b120f9690
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-11-30 23:32:56 -0800

    Started making simple geometry "library".

---
+++ src/common/geom.c
@@ -0,0 +1,69 @@
+#include <math.h>
+
+#include "geom.h"
+#include "point.h"
+
+
+double
+geom_triangle_area(const point2d_t* p1, const point2d_t* p2,
+                   const point2d_t* p3) {
+  const double a = point2d_distance(p1, p2);
+  const double b = point2d_distance(p2, p3);
+  const double c = point2d_distance(p3, p1);
+  const double s = (a + b + c) / 2;
+  return sqrt(s * (s-a) * (s-b) * (s-c));
+}
+
+double
+geom_quad_area(const point2d_t* p1, const point2d_t* p2, const point2d_t* p3,
+               const point2d_t* p4) {
+  double area = geom_triangle_area(p1, p2, p3) + geom_triangle_area(p1, p3, p4);
+  if (geom_segs_intersect(p1, p3, p2, p4)) {
+    area /= 2;
+  }
+  return area;
+}
+
+
+
+
+
+char
+geom_segs_intersect(const point2d_t* a1, const point2d_t* a2,
+                    const point2d_t* b1, const point2d_t* b2) {
+
+  // Method taken from:
+  // http://stackoverflow.com/questions/563198/
+  //              how-do-you-detect-where-two-line-segments-intersect
+
+  point2d_t r = { a2->x - a1->x, a2->y - a1->y };
+  point2d_t s = { b2->x - b1->x, b2->y - b1->y };
+
+  // Some pre-computing.
+  const double r_s = vec_cross_prod_mag(&r, &s);   // r x s
+  point2d_t q_p;
+  vec_sub(&q_p, b1, a1);                         // q - p
+  double q_p_r = vec_cross_prod_mag(&q_p, &r);
+
+  // Preliminary checks.
+  if (GEOM_EQ_0(r_s)) {
+    if (GEOM_EQ_0(q_p_r)) {
+      // collinear
+    } else {
+      // parallel
+    }
+    return 0;
+  }
+
+  // Rest of the computation.
+  double t = vec_cross_prod_mag(&q_p, &s) / r_s;
+  double u = q_p_r / r_s;
+
+  // Remaining checks.
+  if (GEOM_IN_R(t, 0, 1) && GEOM_IN_R(u, 0, 1)) {
+    return 1;
+  }
+
+  // Intersects the line, not the segment.
+  return 0;
+}
---
+++ src/common/geom.h
@@ -0,0 +1,51 @@
+#ifndef __common_geom_h__
+#define __common_geom_h__
+
+#include <math.h>
+
+#include "point.h"
+
+// Error used to handle number rounding errors.
+#define GEOM_ERR 0.00000001
+
+// Determines if the value is low enough to be considered 0.
+#define GEOM_EQ_0(x) (abs(x) <= GEOM_ERR)
+
+// Determines if a number is in a range.
+#define GEOM_IN_R(x,a,z) ((a) - GEOM_ERR < (x) && (x) < (z) + GEOM_ERR)
+
+// Finds the cross-product of the two lines.
+inline double vec_cross_prod_mag(const point2d_t* u, const point2d_t* v) {
+  return u->x * v->y - v->x * u->y;
+}
+
+// Computes rtn = u (cross) v
+inline void vec_sub(point2d_t* rtn, const point2d_t* u, const point2d_t* v) {
+  rtn->x = u->x - v->x;
+  rtn->y = u->y - v->y;
+}
+
+
+
+// Computes area of triangle using Heron's Formula.
+double
+geom_triangle_area(const point2d_t* p1, const point2d_t* p2,
+                   const point2d_t* p3);
+
+// Computes the area of an arbitrary quadrilateral defined by a,b,c,d.  The
+// order of the point determines the shape of the quadrilateral -- a is
+// connected to be and d, b is connected to a and c, and so on.  Should be able
+// to handle "folded" quads.
+double
+geom_quad_area(const point2d_t* p1, const point2d_t* p2, const point2d_t* p3,
+               const point2d_t* p4);
+
+
+
+// Determines if the line a1 -> a2 intersects the line b1 -> b2.
+char
+geom_segs_intersect(const point2d_t* a1, const point2d_t* a2,
+                    const point2d_t* b1, const point2d_t* b2);
+
+#endif  // __common_geom_h__
+
