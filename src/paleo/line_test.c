a5b8f5e6b773066990bcfe0d67687108a09889a5
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-12-01 21:28:28 -0800

    Started using "test_macros.h" in line test.

--- src/paleo/line_test.c
+++ src/paleo/line_test.c
@@ -6,24 +6,8 @@
 #include <assert.h>
 
 #include "common/geom.h"
+#include "test_macros.h"
 #include "line_test.h"
-
-
-
-// Convenience macro to set up the context's result as having failed.
-#define SET_FAIL(msg, args...) { \
-  const int room = strlen(msg) + 100; \
-  context.result->fmsg = realloc( \
-      context.result->fmsg, room * sizeof(char)); \
-  if (room <= snprintf(context.result->fmsg, room, msg, args)) { \
-    fprintf(stderr, "Wrote too many bytes."); \
-    assert(0); \
-  } \
-  context.result->possible = 0; \
-}
-
-// Convenience macro that does the same as SET_FAIL and then returns the result.
-#define SET_FAIL_RTN(msg, args...) SET_FAIL(msg, args) return;
 
 
 
@@ -45,7 +29,7 @@ void line_test_deinit() { free(context.result); }
 ////////////////////////////////////////////////////////////////////////////////
 
 // Resets the context for testing.
-static void _reset(const paleo_stroke_t* stroke) {
+static inline void _reset(const paleo_stroke_t* stroke) {
   context.stroke = stroke;
 }
 
