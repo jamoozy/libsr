cea1671e5bc939b4d4ebaa02b7533a9a6469d675
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-12-04 13:07:09 -0800

    More labels to Paleo threshs.

--- src/paleo/thresh.h
+++ src/paleo/thresh.h
@@ -11,8 +11,8 @@
 #define PALEO_THRESH_H  10.25     // Line FA/len max ratio.
 #define PALEO_THRESH_I   0.0036   // Pline LSE max.
 #define PALEO_THRESH_J   6.0      // min DCR for Pline.
-#define PALEO_THRESH_K   0.8
-#define PALEO_THRESH_L  30.0
+#define PALEO_THRESH_K   0.8      // Ellipse NDDE min
+#define PALEO_THRESH_L  30.0      // Ellipse maj-axis len req.
 #define PALEO_THRESH_M   0.33
 #define PALEO_THRESH_N  16.0
 #define PALEO_THRESH_O   0.425
