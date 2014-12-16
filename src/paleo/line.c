9a60f097a6b5d056ee14b61aaaf1449897c3c023
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-12-16 14:28:17 -0800

    Merged line.[ch] with line_test.[ch] for simplicity.

--- src/paleo/line.c
+++ src/paleo/line.c
@@ -1,22 +1,37 @@
+#include <stdio.h>
 #include <string.h>
+#include <strings.h>
+#include <values.h>
+#include <math.h>
+#include <assert.h>
 
+#include "common/geom.h"
+#include "test_macros.h"
 #include "line.h"
 
-line_t* line_create() {
-  line_t* self = calloc(1, sizeof(line_t));
+
+
+////////////////////////////////////////////////////////////////////////////////
+// ---------------------------- Shape Functions ----------------------------- //
+////////////////////////////////////////////////////////////////////////////////
+
+pal_line_t* pal_line_create() {
+  pal_line_t* self = calloc(1, sizeof(pal_line_t));
   self->pts = calloc(self->num = 2, sizeof(point2d_t*));
   return self;
 }
 
-line_t* line_create_points_with_points(const point2d_t* a, const point2d_t* b) {
-  line_t* self = line_create();
+pal_line_t* pal_line_create_points_with_points(
+    const point2d_t* a, const point2d_t* b) {
+  pal_line_t* self = line_create();
   memcpy(&(self->pts[0]), a, sizeof(point2d_t));
   memcpy(&(self->pts[1]), b, sizeof(point2d_t));
   return self;
 }
 
-line_t* line_create_points_with_longs(long ax, long ay, long bx, long by) {
-  line_t* self = line_create();
+pal_line_t* pal_line_create_points_with_longs(
+    long ax, long ay, long bx, long by) {
+  pal_line_t* self = line_create();
   self->pts[0].x = ax;
   self->pts[0].y = ay;
   self->pts[1].x = bx;
@@ -24,7 +39,205 @@ line_t* line_create_points_with_longs(long ax, long ay, long bx, long by) {
   return self;
 }
 
-void line_destroy(line_t* self) {
-  free(self->pts);
-  free(self);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// ----------------------------- Test Functions ----------------------------- //
+////////////////////////////////////////////////////////////////////////////////
+
+// The line test's context (used by most functions here).
+static pal_line_context_t context;
+
+// Does the line segment test on the ranges provided.
+//   first_i: Index (incl.) of the first point to use.
+//   last_i: Index (excl.) of the last point to use.
+static inline void _line_test(int first_i, int last_i);
+
+const pal_line_result_t* pal_line_test(const pal_stroke_t* stroke) {
+  _reset(stroke);
+  context.result = realloc(context.result, sizeof(line_result_t));
+  if (stroke->num_crnrs == 2 || stroke->num_crnrs == 3) {
+    _line_test(0, stroke->num_pts);
+  }
+  return context.result;
+}
+
+const pal_line_result_t* pal_pline_test(const pal_stroke_t* stroke) {
+  _reset(stroke);
+
+  // Check DCR value.
+  if (stroke->dcr < PAL_THRESH_J) {
+    SET_FAIL_ARR(0, "Stroke DCR val too low: %.2f < %.2f",
+        stroke->dcr, PAL_THRESH_J);
+    return context.result;
+  }
+
+  // Do the line test for each sub-line.
+  context.result = realloc(context.result,
+      stroke->num_crnrs * sizeof(pal_line_result_t));
+  double avg_lse = 0;   // also compute average LSE
+  for (int i = 1; i < stroke->num_crnrs; i++) {
+    _line_seg_test(stroke->crnrs[i-1]->i, stroke->crnrs[i]->i);
+    if (!context.result->possible) {  // Each sub-seg must pass.
+      SET_FAIL_ARR(0, "Does not pass line test in sub-seg %d", i);
+      return context.result;
+    }
+
+    memcpy(&context.result[i], context.result, sizeof(pal_line_result_t));
+    avg_lse += context.result->lse;
+  }
+
+  // Compute avg LSE and verify it's okay.
+  bzero(context.result, sizeof(pal_line_result_t));
+  context.result->lse = avg_lse / stroke->num_crnrs;
+  if (context.result->lse >= PAL_THRESH_I) {
+    SET_FAIL_ARR(0, "Avg LSE too high: %.2f >= %.2f",
+        context.result->lse, PAL_THRESH_I);
+    return context.result;
+  }
+
+  // Everything checks out.  Create the line and return it.
+  context.result->possible = 1;
+  context.result->line.num = stroke->num_crnrs;
+  context.result->line.pts = calloc(
+      context.result->line.num, sizeof(point2d_t));
+  for (int i = 0; i < stroke->num_crnrs; i++) {
+    memcpy(&context.result->line.pts[i], stroke->crnrs[i], sizeof(point2d_t));
+  }
+  return context.result;
+}
+
+// Creates the best fit line segment between the two point indexes and stores it
+// in the context.
+static inline void _best_fit_line_seg(int first_i, int last_i);
+
+// Computes the projection of p to the ideal line.
+//   proj: Return value (projection)
+//   p: The point to project.
+static inline void _projection_to_ideal(point2d_t* proj, const point2d_t* p);
+
+// Computes the orthogonal distance from the point to the ideal line.
+static inline double _distance_to_ideal(const point2d_t* p);
+
+static inline void  _line_seg_test(int first_i, int last_i) {
+  // Reset 0th 
+  bzero(context.result, sizeof(pal_line_result_t));
+  context.result->possible = 1;
+
+  _best_fit_line_seg(first_i, last_i);
+
+  double px_len = 0;  // Length of this sub-stroke.
+  double od2 = 0;     // Orthogonal distance squared.
+  for (int i = first_i; i < last_i; i++) {
+    double d = _distance_to_ideal(&context.stroke->pts[i].p2d);
+    od2 += d * d;
+    if (i > first_i) {
+      px_len += point2d_distance(
+          &context.stroke->pts[i-1].p2d, &context.stroke->pts[i].p2d);
+    }
+  }
+  context.result->lse = od2 / px_len;
+  if (context.result->lse >= PAL_THRESH_G) {
+    SET_FAIL_RTN_ARR(0, "Line LSE too large: %.2f >= %.2f",
+        context.result->lse, PAL_THRESH_G);
+  }
+
+  context.result->fa = 0;
+  for (int i = first_i + 1; i < last_i; i++) {
+    // Compute the projections to the line.
+    point2d_t proj_a, proj_b;
+    _projection_to_ideal(&proj_a, &context.stroke->pts[i-1].p2d);
+    _projection_to_ideal(&proj_b, &context.stroke->pts[i].p2d);
+
+    // Add quad area to feature area (order is important!).
+    context.result->fa += geom_quad_area(&proj_b, &proj_a,
+        &context.stroke->pts[i-1].p2d, &context.stroke->pts[i].p2d);
+  }
+
+  if (context.result->fa / px_len >= PAL_THRESH_H) {
+    SET_FAIL_RTN_ARR(0, "FA too large: %.2f / %.2f = %.2f >= %.2f", context.result->fa,
+        px_len, context.result->fa / px_len, PAL_THRESH_H);
+  }
+
+  // Everything checks out.  Create the line and return.
+  context.result->line.num = 2;
+  context.result->line.pts = calloc(2, sizeof(point2d_t));
+  memcpy(&context.result->line.pts[0],
+      &context.stroke->pts[first_i], sizeof(point2d_t));
+  memcpy(&context.result->line.pts[1],
+      &context.stroke->pts[last_i-1], sizeof(point2d_t));
+}
+
+static inline void _best_fit_line_seg(int first_i, int last_i) {
+  assert(0 <= first_i && first_i < last_i && last_i <= context.stroke->num_pts);
+
+  // Used the algorithm found here:
+  //   http://faculty.cs.niu.edu/~hutchins/csci230/best-fit.htm
+  long sum_x = 0, sum_y = 0, sum_x2 = 0, sum_xy = 0;
+  for (int i = first_i; i < last_i; i++) {
+    sum_x += context.stroke->pts[i].x;
+    sum_y += context.stroke->pts[i].y;
+    sum_x2 += context.stroke->pts[i].x * context.stroke->pts[i].x;
+    sum_xy += context.stroke->pts[i].x * context.stroke->pts[i].y;
+  }
+
+  const int num = last_i - first_i;
+  double x_mean = sum_x / num;
+  double y_mean = sum_y / num;
+
+  // The equivalent of the return value -- assigning values to the ideal line.
+  double denom = (sum_x2 - sum_x * x_mean);
+  if (abs(denom) > 0.0000001) {  // Some number very close to 0.
+    context.ideal_line.slope = (sum_xy - sum_x * y_mean) / denom;
+    context.ideal_line.y_int = y_mean - context.ideal_line.slope * x_mean;
+    context.ideal_line.p0.x = 0;
+    context.ideal_line.p0.y = context.ideal_line.y_int;
+  } else {  // Avoid div-by-0.
+    context.ideal_line.slope = DBL_MAX;
+    context.ideal_line.y_int = DBL_MAX;
+
+    // X coordinate of this line should be average of all the points in the
+    // line.
+    context.ideal_line.p0.x = 0;
+    context.ideal_line.p0.y = LONG_MAX;
+    for (int i = first_i; i < last_i; i++) {
+      context.ideal_line.p0.x += context.stroke->pts[i].x;
+    }
+    context.ideal_line.p0.x /= num;
+  }
+
+  context.ideal_line.theta = atan2(
+    context.stroke->pts[last_i].y - context.stroke->pts[first_i].y,
+    context.stroke->pts[last_i].x - context.stroke->pts[first_i].x);
+}
+
+static inline double _distance_to_ideal(const point2d_t* p) {
+  point2d_t proj;   // projection return value.
+  _projection_to_ideal(&proj, p);
+  return point2d_distance(&proj, p);
+}
+
+static inline void _projection_to_ideal(point2d_t* proj, const point2d_t* p) {
+  assert(context.ideal_line.slope != 0 || context.ideal_line.y_int != 0);
+
+  // Avoid div-by-0.
+  if (context.ideal_line.slope >= DBL_MAX) {
+    proj->x = context.ideal_line.p0.x;
+    proj->y = p->y;
+  } else {
+    const double m_x = 1;
+    const double m_y = context.ideal_line.slope;
+
+    // This is the dividend of two dot products:
+    //    v . s / s . s
+    // where v is the vector from p0 to p, and u is the vector defined by the
+    // slope: i.e., <1,m>
+#define S (context.ideal_line.slope)
+#define P (context.ideal_line.p0)
+    double dist = ((p->x - P.x) + (p->y - P.y) * S) / (1 + S * S);
+    proj->x = P.x + dist;
+    proj->y = P.y + dist * S;
+#undef P
+#undef S
+  }
 }
--- src/paleo/line.h
+++ src/paleo/line.h
@@ -1,31 +1,87 @@
-#ifndef __line_h__
-#define __line_h__
+#ifndef __pal_line_h__
+#define __pal_line_h__
 
 #include "common/point.h"
+
+#include "paleo.h"
+#include "line.h"
 
 // A line with some number of joints.
 typedef struct {
   size_t num;      // The number of points, including start and end.
   point2d_t* pts;  // The points.  Must be at least 2.
-} line_t;
+} pal_line_t;
+
+// The results specific to a line test.
+typedef struct {
+  PALEO_TEST_RESULT_UNION;
+  pal_line_t line;
+} pal_line_result_t;
+
+// Context needed to perform the line test.
+typedef struct {
+  const paleo_stroke_t* stroke;
+  struct {  // The ideal line.
+    double slope;  // slope
+    double y_int;  // Y-intercept
+    point2d_t p0;  // First point in the ideal line.
+    double theta;  // Angle of the line (from p0).
+  } ideal_line;
+
+  // Result of all line tests performed by the last call to line_set_test or
+  // poly_line_test.  For the simple line test there will be only one result
+  // here -- the result of the single line test performed.  For the poly line,
+  // there will be num_crnrs tests here.  The 0th will be the final poly line
+  // test, and the nth will be the line segment test for the nth subsegment of
+  // the line.
+  pal_line_result_t *result;
+} pal_line_context_t;
+
+
+
+
 
 // Creates a new line from 0,0 to 0,0.
-line_t* line_create();
+pal_line_t* pal_line_create();
 
 // Creates a line with the given points.
 //   a: The starting point of the line.
 //   b: The ending point of the line.
-line_t* line_create_points_with_points(const point2d_t* a, const point2d_t* b);
+pal_line_t* pal_line_create_points_with_points(
+    const point2d_t* a, const point2d_t* b);
 
 // Creates a line containing newly created points with the given coordinates.
 //   ax: The starting point's X coord.
 //   ay: The starting point's Y coord.
 //   bx: The ending point's X coord.
 //   by: The ending point's Y coord.
-line_t* line_create_points_with_longs(long ax, long ay, long bx, long by);
+pal_line_t* pal_line_create_points_with_longs(
+    long ax, long ay, long bx, long by);
 
 // Frees the memory in a line.
 //   self: The line to free.
-void line_destroy(line_t* self);
+static inline void pal_line_destroy(pal_line_t* self) {
+  free(self->pts);
+  free(self);
+}
 
-#endif // __line_h__
+
+
+
+
+// Does the line test on the paleo stroke.
+//   stroke: The stroke to test.
+const pal_line_result_t* pal_line_test(const pal_stroke_t* stroke);
+
+// Does the poly line test on the paleo stroke.
+//   stroke: The stroke to test.
+const pal_line_result_t* pal_pline_test(const pal_stroke_t* stroke);
+
+// Initialize the line test.
+static inline void line_init() { bzero(&context, sizeof(line_test_context_t)); }
+
+// De-initializes the line test by freeing its memory.
+static inline void line_deinit() { free(context.result); }
+
+
+#endif // __pal_line_h__
