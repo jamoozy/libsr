e9b6d91a5e0398f78bb95e14fc92d7c1483f97c0
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-12-16 08:54:08 -0800

    Made simple helix create/destroy.

--- src/paleo/helix.c
+++ src/paleo/helix.c
@@ -2,6 +2,6 @@
 #include "helix.h"
 
 
-helix_t* helix_create() { }
+helix_t* helix_create() { return calloc(1, sizeof(helix_t)); }
 
-void helix_destroy(helix_t* self) { }
+void helix_destroy(helix_t* self) { free(self); }
