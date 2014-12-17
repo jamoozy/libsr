855775de7098cf1c4005d8af061b190a843112af
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-12-17 13:29:13 -0800

    Combined arc into one c/h pair.

--- src/paleo/arc.h
+++ src/paleo/arc.h
@@ -1,7 +1,9 @@
-#ifndef __arc_h__
-#define __arc_h__
+#ifndef __pal_arc_h__
+#define __pal_arc_h__
 
 #include "common/point.h"
+#include "paleo.h"
+
 
 // A arc with some number of joints.
 typedef struct {
@@ -9,17 +11,48 @@ typedef struct {
   point2d_t center;   // The center of the arc.
   double angle;       // Angle traversed from pts[0] to pts[1].
                       // (implies whether cw or ccw)
-} arc_t;
+} pal_arc_t;
+
+// The results specific to an arc test.
+typedef struct {
+  PAL_TEST_RESULT_UNION;
+  double fae;     // Feature Area Error.
+  pal_arc_t arc;
+} pal_arc_result_t;
+
+// Context needed to perform the arc test.
+typedef struct {
+  const pal_stroke_t* stroke;
+  struct {              // The ideal arc.
+    point2d_t center;   // Center of arcllipse.
+    double r;           // Arc radius.
+    double area;        // Area.
+  } ideal;
+  pal_arc_result_t result;  // Result of the arc test.
+} pal_arc_context_t;
+
 
 // Creates a new arc with the given
-arc_t* arc_create(const point2d_t* p1, const point2d_t* p2,
+pal_arc_t* pal_arc_create(const point2d_t* p1, const point2d_t* p2,
     const point2d_t* c, double angle);
 
 // Populates a pre-allocated arc.
-void arc_populate(arc_t* self, const point2d_t* p1, const point2d_t* p2,
+void pal_arc_populate(pal_arc_t* self, const point2d_t* p1, const point2d_t* p2,
     const point2d_t* c, double angle);
 
 // Destroys the arc by freeing its memory.
-void arc_destroy(arc_t*);
+void pal_arc_destroy(pal_arc_t*);
 
-#endif // __arc_h__
+
+
+// Initialize the arc test.
+void pal_arc_init();
+
+// De-initializes the arc test by freeing its memory.
+void pal_arc_deinit();
+
+// Does the arc test on the paleo stroke.
+//   stroke: The stroke to test.
+const pal_arc_result_t* pal_arc_test(const pal_stroke_t* stroke);
+
+#endif // __pal_arc_h__
