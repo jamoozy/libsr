e8c35874b2bb6e1d45e01e00e74dd258813cc1a2
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-12-17 13:55:01 -0800

    Combined helix.

--- src/paleo/helix.c
+++ src/paleo/helix.c
@@ -1,21 +1,115 @@
-
+#include <config.h>
+#include <values.h>
 #include <math.h>
 
+#include "common/geom.h"
+#include "common/util.h"
+
+#include "thresh.h"
+#include "test_macros.h"
 #include "helix.h"
 
 
-helix_t* helix_create() { return calloc(1, sizeof(helix_t)); }
+pal_helix_t* pal_helix_create() { return calloc(1, sizeof(pal_helix_t)); }
 
-void helix_destroy(helix_t* self) { free(self); }
+void pal_helix_destroy(pal_helix_t* self) { free(self); }
 
-void helix_compute_points(const helix_t* self, point2d_t* p, int num) {
+void pal_helix_compute_points(const pal_helix_t* self, point2d_t* p, int num) {
   for (int i = 0; i < num; i++) {
-    helix_compute_point(self, &p[i], i / (num-1.0));
+    pal_helix_compute_point(self, &p[i], i / (num-1.0));
   }
 }
 
-void helix_compute_point(const helix_t* self, point2d_t* p, double t) {
+void pal_helix_compute_point(const pal_helix_t* self, point2d_t* p, double t) {
   double a = self->theta_i + self->cw * self->theta_t * t;
   p->x = self->c[0].x * (1-t) + self->c[1].x * t + self->r * cos(a);
   p->y = self->c[0].y * (1-t) + self->c[1].y * t + self->r * sin(a);
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// ------------------------------ Helix Shape ------------------------------- //
+////////////////////////////////////////////////////////////////////////////////
+
+// The context used for shape recognition.
+static pal_helix_context_t context;
+
+void pal_helix_init() {
+  bzero(&context, sizeof(pal_helix_context_t));
+}
+
+void pal_helix_deinit() { }
+
+static inline void _reset(const pal_stroke_t* stroke) {
+  _RESET(stroke);
+  //bzero(&context, sizeof(pal_helix_context_t));
+  //context.stroke = stroke;
+}
+
+const pal_helix_result_t* pal_helix_test(const pal_stroke_t* stroke) {
+  _reset(stroke);
+
+  CHECK_RTN_RESULT(stroke->overtraced, "Stroke not overtraced.");
+  CHECK_RTN_RESULT(stroke->ndde > PALEO_THRESH_K,
+      "NDDE too low: %.2f <= K (%.2f)", stroke->ndde, PALEO_THRESH_K);
+
+  // Check that this looks helix-like.
+  double ep_dist = point2d_distance(
+      &stroke->pts[0].p2d, &stroke->pts[stroke->num_pts-1].p2d);
+  CHECK_RTN_RESULT(ep_dist / stroke->px_length >= PALEO_THRESH_U,
+      "ep_dist (%.2f) / px_len (%.2f) >= U (%.2f)",
+      ep_dist, stroke->px_length, PALEO_THRESH_U);
+
+  // All tests pass, build the helix.
+
+  // Compute "major axis"
+  struct {
+    int i;
+    int j;
+    double dist;
+    double angle;
+  } maj = { -1, -1, DBL_MIN };
+  for (int i = 0; i < stroke->num_pts; i++) {
+    for (int j = i+1; j < stroke->num_pts; j++) {
+      double dist = point2d_distance(&stroke->pts[i].p2d, &stroke->pts[j].p2d);
+      if (dist > maj.dist) {
+        maj.i = i;
+        maj.j = j;
+        maj.dist = dist;
+      }
+    }
+  }
+  maj.angle = point2d_angle_to(
+      &stroke->pts[maj.i].p2d, &stroke->pts[maj.j].p2d);
+
+  // Compute radius.
+  context.ideal.r = 0;
+  for (int i = 0; i < stroke->num_pts; i++) {
+    context.ideal.r += geom_point_dist_to_line(
+        &stroke->pts[i].p2d, &stroke->pts[maj.i].p2d, &stroke->pts[maj.j].p2d);
+  }
+  context.ideal.r /= stroke->num_pts;
+
+  point2d_t vec = {
+    stroke->pts[maj.j].x - stroke->pts[maj.i].x,
+    stroke->pts[maj.j].y - stroke->pts[maj.i].y
+  };
+  context.result.helix.c[0].x =
+    stroke->pts[maj.i].x + vec.x * context.ideal.r / maj.dist;
+  context.result.helix.c[0].y =
+    stroke->pts[maj.i].x + vec.y * context.ideal.r / maj.dist;
+  context.result.helix.c[1].x =
+    stroke->pts[maj.j].x - vec.x * context.ideal.r / maj.dist;
+  context.result.helix.c[1].y =
+    stroke->pts[maj.j].x - vec.y * context.ideal.r / maj.dist;
+  context.result.helix.r = context.ideal.r;
+  context.result.helix.theta_i = point2d_angle_to(
+      &context.result.helix.c[0], &stroke->pts[0].p2d);
+  context.result.helix.theta_t =
+    stroke->pts[stroke->num_pts-1].dir - stroke->pts[0].dir;
+  context.result.helix.cw = SGN(context.result.helix.theta_t);
+  context.result.helix.theta_t = abs(context.result.helix.theta_t);
+
+  return &context.result;
 }
--- src/paleo/helix.h
+++ src/paleo/helix.h
@@ -1,26 +1,55 @@
-#ifndef __paleo_helix_h__
-#define __paleo_helix_h__
+#ifndef __pal_helix_h__
+#define __pal_helix_h__
 
 #include "common/point.h"
 
+#include "paleo.h"
+
+// The helix shape.
 typedef struct {
   point2d_t c[2];   // Starting and ending center points.
   double r;         // Radius.
   double theta_i;   // Initial angle.
   double theta_t;   // Angle traversed.
   int cw;           // Clockwise?
-} helix_t;
+} pal_helix_t;
+
+// The helix test result.
+typedef struct {
+  PAL_TEST_RESULT_UNION;
+  pal_helix_t helix;
+} pal_helix_result_t;
+
+// The helix test context.
+typedef struct {
+  const pal_stroke_t* stroke;
+  pal_helix_result_t result;
+} pal_helix_context_t;
+
+
 
 // Creates a new helix.
-helix_t* helix_create();
+pal_helix_t* helix_create();
 
 // Destroys the helix by freeing its memory.
-void helix_destroy(helix_t*);
+//    self: The helix to destroy.
+void pal_helix_destroy(pal_helix_t* self);
 
 // Computes 'num' evenly spaced points.
-void helix_compute_points(const helix_t* self, point2d_t* p, int num);
+void pal_helix_compute_points(const pal_helix_t* self, point2d_t* p, int num);
 
 // Computes a single point at the parametric value 't'.
-void helix_compute_point(const helix_t* self, point2d_t* p, double t);
+void pal_helix_compute_point(const pal_helix_t* self, point2d_t* p, double t);
 
-#endif // __paleo_helix_h__
+
+
+// Initialize the curve test.
+void pal_helix_init();
+
+// De-initializes the curve test.
+void pal_helix_deinit();
+
+// Test whether this is a helix.
+const pal_helix_result_t* pal_helix_test(const pal_stroke_t* stroke);
+
+#endif // __pal_helix_h__
