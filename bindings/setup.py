5213070b1750ed34f86a25533479a7315cfcb374
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-05-25 11:57:16 -0700

    Allow python binding build from any dir.

--- bindings/setup.py
+++ bindings/setup.py
@@ -1,12 +1,22 @@
+import os
 from distutils.core import setup, Extension
+
+# Use absolute paths for source files.  This allows us to run this script from
+# any directory.
+dir_name = os.path.dirname(os.path.realpath(__file__))
+base_dir = os.path.join(dir_name, '..')
+src_dir = os.path.join(base_dir, 'src')
 
 libsr = Extension('libsr',
                   define_macros = [('MAJOR_VERSION', '0'),
                                    ('MINOR_VERSION', '1')],
-                  include_dirs = ['/usr/local/include'],
-                  libraries = ['../src/libsr.a'],
+                  include_dirs = ['/usr/local/include',
+                                  os.path.join(src_dir, 'common')],
+                  libraries = [os.path.join(src_dir, lib)
+                               for lib in ['libsr.a']],
                   library_dirs = ['/usr/local/lib'],
-                  sources = ['libsrmodule.c'])
+                  sources = [os.path.join(dir_name, src)
+                             for src in ['libsrmodule.c']])
 
 setup (name = 'libsr',
        version = '0.1',
