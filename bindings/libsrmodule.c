3d82c8f3db8d6c031d17bb31c72801cd87c8c921
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-05-25 21:30:37 -0700

    Start working on getting functionality in Python bindings.
    No luck yet -_-.

--- bindings/libsrmodule.c
+++ bindings/libsrmodule.c
@@ -1,42 +1,128 @@
 #include <Python.h>
+#include <structmember.h>
+
 #include <stdarg.h>
+
+#include "stroke.h"
 
 
 typedef struct {
   PyObject_HEAD
-  /* fields ... */
-} libsr_StrokeObject;
+  stroke_t* stroke;   // The underlying libsr stroke_t in src/common/
+} libsr_Stroke;
+
+static PyObject *
+Stroke_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
+  libsr_Stroke* self = (libsr_Stroke*)type->tp_alloc(type, 0);
+  if (self != NULL) {
+    self->stroke = stroke_create(40);
+  }
+  return (PyObject*)self;
+}
+
+static void
+Stroke_dealloc(libsr_Stroke* self) {
+    stroke_destroy(self->stroke);
+    self->ob_type->tp_free((PyObject*)self);
+}
+
+static int
+Stroke_init(libsr_Stroke* self, PyObject* args, PyObject* kwargs) {
+  return 0;
+}
+
+
+static int
+Stroke_add(libsr_Stroke* self, PyObject* args, PyObject* kwargs) {
+  long x = 0, y = 0, t = 0;
+  static char* kwlist[] = {"x", "y", "t", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii|i", kwlist, &x, &y, &t)) {
+    return -1;
+  }
+  stroke_add_timed(self->stroke, x, y, t);
+  return 0;
+}
+
+static int
+Stroke_save(libsr_Stroke* self, PyObject* args, PyObject* kwargs) {
+  const char* fname = NULL;
+  static char* kwlist[] = {"fname", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", kwlist, &fname)) {
+    return -1;
+  }
+  stroke_save(self->stroke, fname);
+  return 0;
+}
+
+//static int
+//Stroke_from_file(libsr_Stroke* self, PyObject* args, PyObject* kwargs) {
+//  const char* fname = NULL;
+//  static char* kwlist[] = {"fname", NULL};
+//  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", kwlist, &fname)) {
+//    return -1;
+//  }
+//  stroke_(self->stroke, fname);
+//  return 0;
+//}
+
+static PyMethodDef Stroke_methods[] = {
+  {"add", (PyCFunction)Stroke_add, METH_VARARGS,
+   ""},
+  {"save", (PyCFunction)Stroke_save, METH_VARARGS,
+   "Saves the stroke to disk."},
+  {NULL, NULL, 0, NULL}
+};
+
 
 static PyTypeObject libsr_StrokeType = {
   PyObject_HEAD_INIT(NULL)
-  0,                           // ob_size (must always be 0)
-  "libsr.Stroke",              // tp_name
-  sizeof(libsr_StrokeObject),  // tp_basicsize
-  0,                           // tp_itemsize
-  0,                           // tp_dealloc
-  0,                           // tp_print
-  0,                           // tp_getattr
-  0,                           // tp_setattr
-  0,                           // tp_compare
-  0,                           // tp_repr
-  0,                           // tp_as_number
-  0,                           // tp_as_sequence
-  0,                           // tp_as_mapping
-  0,                           // tp_hash
-  0,                           // tp_call
-  0,                           // tp_str
-  0,                           // tp_getattro
-  0,                           // tp_setattro
-  0,                           // tp_as_buffer
-  Py_TPFLAGS_DEFAULT,          // tp_flags
+  0,                     // ob_size (must always be 0)
+  "libsr.Stroke",        // tp_name
+  sizeof(libsr_Stroke),  // tp_basicsize
+  0,                     // tp_itemsize
+  (destructor)Stroke_dealloc, // tp_dealloc
+  0,                     // tp_print
+  0,                     // tp_getattr
+  0,                     // tp_setattr
+  0,                     // tp_compare
+  0,                     // tp_repr
+  0,                     // tp_as_number
+  0,                     // tp_as_sequence
+  0,                     // tp_as_mapping
+  0,                     // tp_hash
+  0,                     // tp_call
+  0,                     // tp_str
+  0,                     // tp_getattro
+  0,                     // tp_setattro
+  0,                     // tp_as_buffer
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
 
   // Python docstring:
   "Holds a sequence of timestamped points.",
+
+  0,                      // tp_traverse
+  0,                      // tp_clear
+  0,                      // tp_richcompare
+  0,                      // tp_weaklistoffset
+  0,                      // tp_iter
+  0,                      // tp_iternext
+  Stroke_methods,         // tp_methods
+  0,                      // tp_members
+  0,                      // tp_getset
+  0,                      // tp_base
+  0,                      // tp_dict
+  0,                      // tp_descr_get
+  0,                      // tp_descr_set
+  0,                      // tp_dictoffset
+  (initproc)Stroke_init,  // tp_init
+  0,                      // tp_alloc
+  Stroke_new,             // tp_new
 };
 
 static PyMethodDef libsr_methods[] = {
-  {NULL}  // Sentinel
+  {NULL, NULL, 0, NULL}  // Sentinel
 };
+
 
 
 #ifndef PyMODINIT_FUNC       // declarations for DLL import/export
@@ -44,15 +130,13 @@ static PyMethodDef libsr_methods[] = {
 #endif
 PyMODINIT_FUNC
 initlibsr(void) {
-  PyObject* m;
-
-  libsr_StrokeType.tp_new = PyType_GenericNew;
   if (PyType_Ready(&libsr_StrokeType) < 0)
     return;
 
-  m = Py_InitModule3("libsr", libsr_methods,
-                     "Sketch recognition module written by Andrew \"Jamoozy\""
-                     " C. Sabisch.  Distributed under the GPLv3.");
+  PyObject* m = Py_InitModule3(
+      "libsr", libsr_methods,
+      "Sketch recognition module written by Andrew \"Jamoozy\" C. Sabisch. "
+      "Distributed under the GPLv3.");
 
   Py_INCREF(&libsr_StrokeType);
   PyModule_AddObject(m, "Stroke", (PyObject*)&libsr_StrokeType);
--- bindings/setup.py
+++ bindings/setup.py
@@ -1,8 +1,10 @@
 import os
 from distutils.core import setup, Extension
 
+
 # Use absolute paths for source files.  This allows us to run this script from
 # any directory.
+build_dir = os.path.join(os.getcwd(), '..')
 dir_name = os.path.dirname(os.path.realpath(__file__))
 base_dir = os.path.join(dir_name, '..')
 src_dir = os.path.join(base_dir, 'src')
@@ -12,11 +14,9 @@ libsr = Extension('libsr',
                                    ('MINOR_VERSION', '1')],
                   include_dirs = ['/usr/local/include',
                                   os.path.join(src_dir, 'common')],
-                  libraries = [os.path.join(src_dir, lib)
-                               for lib in ['libsr.a']],
+                  extra_link_args = [os.path.join(build_dir, 'src', 'libsr.la')],
                   library_dirs = ['/usr/local/lib'],
-                  sources = [os.path.join(dir_name, src)
-                             for src in ['libsrmodule.c']])
+                  sources = [os.path.join(dir_name, 'libsrmodule.c')])
 
 setup (name = 'libsr',
        version = '0.1',
