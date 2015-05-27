4b0a93b6d7423878e6dd28f3afd7350c27c0808b
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-05-27 01:34:31 -0700

    Added correct "clean" target for Python bindings.

--- bindings/setup.py
+++ bindings/setup.py
@@ -16,14 +16,18 @@ dir_name = os.path.dirname(os.path.realpath(__file__))
 base_dir = os.path.join(dir_name, '..')
 src_dir = os.path.join(base_dir, 'src')
 
+LIBSR_SO = os.path.join(build_dir, 'src', '.libs', 'libsr.so')
+LIBSR_SO_DIR = os.path.dirname(LIBSR_SO)
+
 libsr = Extension('libsr',
                   define_macros=VERSION,
                   include_dirs=['/usr/local/include',
                                 os.path.join(src_dir, 'common')],
-                  extra_link_args=[os.path.join(build_dir,
-                                                'src', '.libs', 'libsr.so')],
-                  library_dirs=['/usr/local/lib'],
-                  sources=[os.path.join(dir_name, 'libsrmodule.c')])
+                  extra_link_args=[LIBSR_SO],
+                  library_dirs=[LIBSR_SO_DIR, '/usr/local/lib'],
+                  runtime_library_dirs=[LIBSR_SO_DIR, '/usr/local/lib'],
+                  sources=[os.path.join(dir_name, 'libsrmodule.c')],
+                  depends=[LIBSR_SO])
 
 setup(name='libsr',
       version='0.1',
