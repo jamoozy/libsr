36dfe5214b7963b11ca70c58eab6d45d6bb8f194
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-06-21 02:21:59 -0700

    docs (mainly)
    ... also got rid of "mouse_down" -- turns out it's unnecessary.

--- tools/collection.py
+++ tools/collection.py
@@ -23,52 +23,86 @@ except ImportError:
 class Canvas(QtGui.QGraphicsView):
   '''Canvas to collect and display strokes.'''
 
-  mouse_down = False
-
   def __init__(self):
+    '''Initializes the canvas.'''
     super(Canvas, self).__init__(QtGui.QGraphicsScene())
     self.scene().setSceneRect(0, 0, 800, 600)
     self.setBackgroundBrush(QtCore.Qt.white)
     self.setWindowTitle('Canvas')
     self.show()
 
+  def resizeEvent(self, e):
+    '''See :meth:`QGraphicsView.mouseResizeEvent`.'''
+    self.scene().setSceneRect(0, 0, e.size().width(), e.size().height())
+
   def _append_pos(self, x, y):
-    assert self.mouse_down, 'error: mouse not down'
+    '''Append the position to the most-recently drawn stroke.
+
+    Args:
+      x, float: X-position of mouse event.
+      y, float: Y-position of mouse event.
+    '''
     rect = self.scene().items()[0].add(x, y)
 
   def mousePressEvent(self, e):
-    self.mouse_down = True
+    '''See :meth:`QGraphicsView.mousePressEvent`.'''
     self.scene().addItem(StrokeGraphiscItem(e.x(), e.y()))
 
   def mouseMoveEvent(self, e):
+    '''See :meth:`QGraphicsView.mouseMoveEvent`.'''
     self._append_pos(e.x(), e.y())
 
   def mouseReleaseEvent(self, e):
+    '''See :meth:`QGraphicsView.mouseReleaseEvent`.'''
     self._append_pos(e.x(), e.y())
-    self.mouse_down = False
 
 
 class StrokeGraphiscItem(QtGui.QGraphicsItem):
-  '''Draws an internal libsr.Stroke object.'''
+  '''Draws an underlying libsr.Stroke object.'''
   DRAW_BUF = 5   # Small buffer to get some ... well ... buffer around rects.
 
   def __init__(self, x, y):
+    '''Creates a new graphics item (and underlying stroke) starting at (x, y).
+
+    Args:
+      x, float: X-position of mouse event.
+      y, float: Y-position of mouse event.
+    '''
     super(StrokeGraphiscItem, self).__init__()
     self.stroke = libsr.Stroke()
     self._add_scene_point(x, y)
 
   def _add_scene_point(self, x, y):
+    '''Adds a scene point to the underlying libsr.Stroke object.
+
+    Args:
+      x, float: X-coordinate of mouse event.
+      y, float: Y-coordinate of mouse event.
+    '''
     self.stroke.add(int(x), int(y))
     self.prepareGeometryChange()
 
   def add(self, x, y):
+    '''Adds a new point to the underlying strokes.  Also registers that this has
+    added a new point, and thus needs to be redrawn.
+
+    Args:
+      x, float: X-coordinate of mouse event.
+      y, float: Y-coordinate of mouse event.
+    '''
     pt = self.mapToScene(x, y)
     self._add_scene_point(int(pt.x()), int(pt.y()))
 
   def boundingRect(self):
+    '''The rectangle bounding all the points in the underlying stroke.
+
+    Returns:
+      The rectangle containing the underlying stroke.
+    '''
     return QtCore.QRectF(*self.stroke.bbox)
 
   def paint(self, painter, option, widget):
+    '''See QGraphicsItem.paint().'''
     pen = QtGui.QPen(QtCore.Qt.SolidLine)
     pen.setColor(QtCore.Qt.black)
     pen.setWidth(1)
@@ -81,6 +115,7 @@ class StrokeGraphiscItem(QtGui.QGraphicsItem):
 
 
 def main(*args):
+  '''Proverbial "main()" method!'''
   app = QtGui.QApplication(list(args))
   c = Canvas()
   sys.exit(app.exec_())
@@ -88,4 +123,3 @@ def main(*args):
 
 if __name__ == '__main__':
   main(*sys.argv)
-
