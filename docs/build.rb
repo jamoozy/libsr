6ca8933cceee962761dfdc9db88c4294070fc88b
Andrew "Jamoozy" C. Sabisch <jamoozy@gmail.com>
2015-08-01 15:56:47 -0700

    Started making convenience scripts.
    One is for buliding docs -- ALL the docs.
    One is for running tests -- both in and out of gdb.

--- docs/build.rb
+++ docs/build.rb
@@ -7,18 +7,20 @@ require 'optparse'
 
 
 def run(cmd)
-  puts "Running: #{cmd}" if $options[:verbose]
+  puts %{Running: "#{cmd}"} if $options[:verbose]
   `#{cmd}`
 end
 
 def parse! args
+  # Default values.
   $options = {
     :verbose => false,
     :indir => nil,
     :outdir => nil,
+    :subst => '',
   }
   OptionParser.new do |opts|
-    opts.banner = "Usage: #$0 [-o <dir>|--output[=]<dir>]"
+    opts.banner = "Usage: #$0 [options]"
 
     opts.on('-v', '--verbose', 'Run verbosely.') do |v|
       $options[:verbose] = v
@@ -29,13 +31,39 @@ def parse! args
     opts.on('-o', '--outdir DIR', 'Set output directory') do |dir|
       $options[:outdir] = dir
     end
+    opts.on('-s', '--subst S', 'List of comma-separated "k=v" pairs') do |s|
+      $options[:subst] = s.split(',').map{|e| e.split('=')}
+    end
   end.parse!
 end
 
 def build args
   parse! args
 
-  dirs=run(%{for d in `cat #{$options[:indir]}/Doxyfile | grep '\<INPUT\\s\+=' | gawk -F= '{print $2}'` ; do printf "${srcdir}/$$d " ; done})
+  # Find the directory with a "Doxyfile" in it.
+  base_dir = File.dirname(__FILE__)
+  while !File.file?(File.join(base_dir, 'Doxyfile'))
+    base_dir = File.dirname(base_dir)
+  end
+
+  # Get all the variables from the Doxyfile as a Hash.
+  vals = Hash[
+    IO.readlines(File.join(base_dir, 'Doxyfile').reject do |line|
+      line =~ /^\s*#/ or line =~ /^\s*$/
+    end.map do |line|
+      line.split('=').map{|e| e.strip}
+    end.flatten
+  ]
+
+  # Update
+
+  Dir.chdir(base_dir) do
+    cmd = 'doxygen'
+    cmd = "#{} #{cmd}"
+    run(cmd)
+  end
+
+  dirs = run(%{for d in `cat #{$options[:indir]}/Doxyfile | grep '\<INPUT\\s\+=' | gawk -F= '{print $2}'` ; do printf "${srcdir}/$$d " ; done})
 	run(%{(cat #{$options[:indir]}/Doxyfile | sed -r 's/(^INPUT\\s+=\\s+).*/\\1#{dirs}/' ; echo "INPUT = #{dirs}") | doxygen -})
 end
 
---
+++ tests/run.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby -w
+
+require 'rubygems'
+require 'optparse'
+
+def run cmd
+  puts "Running: #{cmd}" if $options[:verbose]
+  `#{cmd}`
+end
+
+def parse! args
+  $options = {
+    :verbosity => 0,    # Kind of a "log level"
+    :debug => false,    # Whether to run in gdb for debugging.
+    :target => nil,     # Default is "all"
+    :builddir => './',  # Root directory where *.o are.
+  }
+
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #$0 [options]"
+
+    opts.on('-v', '--verbose', 'Run verbosely') do |v|
+      $options[:verbosity] += 1
+    end
+    opts.on('-d', '--debug TARGET', 'Run in debug mode (via gdb)') do |target|
+      $options[:debug] = target
+    end
+    opts.on('-b', '--builddir DIR', 'Set build dir.') do |builddir|
+      $options[:builddir] = builddir
+    end
+  end.parse!
+
+  puts 'Warning: no build dir set.' if $options[:builddir] == nil
+end
+
+def run args
+  parse! args
+
+  if $options[:debug] then
+    run(%{CK_FORK=no gdb #{$options[:target]} -ex run})
+  elsif $options[:target]
+    run("./#{$options[:target]}")
+  end
+end
+
+run ARGV if $0 == __FILE__
