c3792e58e8bd4cfae671fb37dc8e6f41b4e1e7a7
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-09-26 15:39:03 -0700

    Finish up paleo bindings.

--- go/paleo.go
+++ go/paleo.go
@@ -5,16 +5,30 @@ import (
 )
 
 // #cgo CFLAGS: -I../src
-// #cgo LDFLAGS: -L../build/src/.libs -lsr
+// #cgo LDFLAGS: -lsr -lm -lcblas -llapacke
 // #include "paleo/paleo.h"
 import "C"
+// Got to add pkg-config stuff!
 
 type Stroke struct {
-  c *C.stroke_t
+  Pts [][3]int64
 }
 
-func recognize(stroke Stroke) (int, error) {
-  palType := C.pal_recognize(stroke.c)
+// Creates a new stroke.
+func NewStroke() Stroke {
+  return Stroke{Pts: make([][3]int64, 0, 100)}
+}
+
+func (s *Stroke) Recognize() (int, error) {
+  // Build stroke.
+  cStrk := C.stroke_create(C.int(len(s.Pts)))
+  defer C.stroke_destroy(cStrk)
+  for _, pt := range s.Pts {
+    C.stroke_add_timed(cStrk, C.long(pt[0]), C.long(pt[1]), C.long(pt[2]))
+  }
+
+  // Recognize it.
+  palType := C.pal_recognize(cStrk)
   if palType <= C.PAL_TYPE_UNRUN || C.PAL_TYPE_NUM <= palType {
     return 0, errors.New("Unknown error.  Sorry!")
   }
