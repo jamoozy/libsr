f96acf91dea149996796b7e73036cc189de6f94a
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-11-02 19:00:06 -0800

    Finished first functionality for $P.

--- src/dollarp/dollarp.c
+++ src/dollarp/dollarp.c
@@ -1,4 +1,4 @@
-
+#include <assert.h>
 #include <math.h>
 #include <string.h>
 #include <values.h>
@@ -6,15 +6,19 @@
 #include "common/geom.h"
 #include "dollarp.h"
 
-static inline void _translate_to_origin(stroke_t* strk, int n) {
+/*! Moves the stroke to be centered at the origin, \f$(0,0)\f$.
+ *
+ * \param strk The stroke.
+ */
+static inline void _translate_to_origin(stroke_t* strk) {
   point2d_t c = { 0, 0 };
   for (int i = 0; i < strk->num; i++) {
     c.x += strk->pts[i].x;
     c.y += strk->pts[i].y;
   }
 
-  c.x /= n;
-  c.y /= n;
+  c.x /= strk->num;
+  c.y /= strk->num;
 
   for (int i = 0; i < strk->num; i++) {
     strk->pts[i].x -= c.x;
@@ -22,7 +26,11 @@ static inline void _translate_to_origin(stroke_t* strk, int n) {
   }
 }
 
-static inline double _scale(stroke_t* strk) {
+/*! Scales the stroke, i.e., normalizes the size.
+ *
+ * \param strk The stroke to normalize.
+ */
+static inline void _scale(stroke_t* strk) {
   point2d_t min = { DBL_MAX, DBL_MAX };
   point2d_t max = { 0, 0 };
 
@@ -40,6 +48,10 @@ static inline double _scale(stroke_t* strk) {
   }
 }
 
+/*! Finds the length of the path along the stroke.
+ * \param strk The stroke.
+ * \return The length along the stroke.
+ */
 static inline double _path_length(const stroke_t* strk) {
   double len = 0;
   for (int i = 1; i < strk->num; i++) {
@@ -48,7 +60,14 @@ static inline double _path_length(const stroke_t* strk) {
   return len;
 }
 
-static inline stroke_t* _resample(stroke_t* strk, int n) {
+/*! Resamples the stroke to have `n` points.
+ *
+ * \param strk The stroke to resample.
+ * \param n The number of points the stroke should have.
+ *
+ * \return The resampled stroke.
+ */
+static inline void _resample(stroke_t* strk, int n) {
   double I = _path_length(strk) / (n - 1);
   double D = 0;
   stroke_t* r_strk = stroke_create(n);
@@ -57,7 +76,7 @@ static inline stroke_t* _resample(stroke_t* strk, int n) {
     point2d_t* curr = &strk->pts[i].p2d;
     point2d_t* prev = &strk->pts[i-1].p2d;
     double d = point2d_distance(curr, prev);
-    if (D + d >= D) {
+    if (D + d >= I) {
       point2d_t q = {
         prev->x + ((I - D) / d) * (curr->x - prev->x),
         prev->y + ((I - D) / d) * (curr->y - prev->y)
@@ -66,64 +85,99 @@ static inline stroke_t* _resample(stroke_t* strk, int n) {
       D = 0;
 
       // Set up for the next curr/prev pair to be q and i-1.
-      memcpy(&strk->pts[i-1], &strk->pts[i], sizeof(point2d_t));
-      memcpy(&strk->pts[i], &q, sizeof(point2d_t));
+      stroke_insert_at(strk, i, q.x, q.y);
       i--;
     } else {
       D += d;
     }
   }
 
-  return r_strk;
+  // Copy the computed resampled stroke to the input stroke and clean up.
+  memcpy(strk, r_strk, sizeof(stroke_t));
+  stroke_destroy(r_strk);
 }
 
-static inline stroke_t* _normalize(stroke_t* strk, int n) {
-  stroke_t* norm_strk = _resample(strk, n);
-  _scale(norm_strk);
-  _translate_to_origin(norm_strk, n);
-  return norm_strk;
+/*! "Normalizes" the stroke to have just `n` points in it centered at the
+ * origin.  Note that this mutates `strk`.
+ *
+ * \param strk The stroke to normalize
+ * \param n The number of points to keep in the stroke.
+ */
+static inline void _normalize(stroke_t* strk, int n) {
+  _resample(strk, n);
+  _scale(strk);
+  _translate_to_origin(strk);
 }
 
-static inline int
-_cloud_dist(const stroke_t* strk, const stroke_t* tmpl, double start, int n) {
+/*! Finds the distance between two clouds.
+ *
+ * From the paper:
+ * <blockquote>
+ * Distance between two clouds. Compute the minimum-cost alignment between
+ * points and `tmpl` starting with point start. Assign decreasing confidence
+ * weights \f$\in [0,1]\f$ to point matchings.
+ * </blockquote>
+ *
+ * \param c1 One point cloud.
+ * \param c2 Another point cloud.
+ * \param start The start index to search from.
+ * \param n The size of the strokes.
+ *
+ * \return The distance between the clouds.
+ */
+static inline double
+_cloud_dist(const stroke_t* c1, const stroke_t* c2, int start) {
   // Effective array of booleans.
-  int* matched = calloc(n, sizeof(char));
+  int* matched = calloc(c1->num, sizeof(char));
   double sum = 0;
   int i = 0;
   int index = -1;
   do {
     double min = DBL_MAX;
-    for (int j = 0; j < n; j++) {
-      if (matched[j]) {
-        continue;
-      }
-
-      double d = point2d_distance(&strk->pts[i].p2d, &tmpl->pts[j].p2d);
-      if (d < min) {
-        min = d;
-        index = j;
+    for (int j = 0; j < c1->num; j++) {
+      if (!matched[j]) {
+        double d = point2d_distance(&c1->pts[i].p2d, &c2->pts[j].p2d);
+        if (d < min) {
+          min = d;
+          index = j;
+        }
       }
     }
 
     matched[index] = 1;
-    double weight = 1 - ((int)(floor(i - start + n)) % (int)(n)) / n;
+    double weight = 1 - ((i - start + c1->num) % c1->num) / c1->num;
     sum += weight * min;
-    i = (i + 1) % n;
+    i = (i + 1) % c1->num;
   } while (i != start);
 
   return sum;
 }
 
+/*! Attempt to match two point clouds.
+ *
+ * From the article:
+ * <blockquote>
+ * Cloud matching function. Match two clouds (points and template) by performing
+ * repeated alignments between their points (each new alignment starts with a
+ * different starting point index \f$i\f$).  Parameter \f$\varepsilon \in
+ * [0,1]\f$ controls the number of tested alignments (\f$n^\varepsilon \in \{1,
+ * 2, \ldots, n\}\f$). Returns the minimum alignment cost.
+ * </blockquote>
+ *
+ * \param self The $P context.
+ * \param c1 A point cloud to compare.
+ * \param c2 Another point cloud to compare.
+ */
 static inline double
-_greedy_cloud_match(const dp_context_t* self, const stroke_t* strk,
-                    const stroke_t* tmpl) {
-  double step = floor(pow(self->norm_size, 1 - self->epsilon));
-  double min = DBL_MAX;
+_greedy_cloud_match(const dp_context_t* self, const stroke_t* c1,
+                    const stroke_t* c2) {
+  assert(c1->num == c2->num);
 
-  for (double i = 0; i < self->norm_size; i += step) {
+  double min = DBL_MAX;
+  for (double i = 0; i < self->n; i += self->step) {
     double d[] = {
-      _cloud_dist(strk, tmpl, i, self->norm_size),
-      _cloud_dist(tmpl, strk, i, self->norm_size)
+      _cloud_dist(c1, c2, (int)round(i)),
+      _cloud_dist(c2, c1, (int)round(i))
     };
     min = MIN(d[0], d[1]);
   }
@@ -136,20 +190,40 @@ _greedy_cloud_match(const dp_context_t* self, const stroke_t* strk,
 //                             "Public" Functions                             //
 ////////////////////////////////////////////////////////////////////////////////
 
-dp_context_t* create_dollarp(size_t norm_size) {
+//! The amount to increase the template size by, each time it needs increasing.
+#define _DP_TMPL_INC 100
+
+dp_context_t* create_dollarp() {
   dp_context_t* self = calloc(1, sizeof(dp_context_t));
-  self->norm_size = norm_size;
+  self->n = DP_DEFAULT_N;
+  dp_set_epsilon(self, DP_DEFAULT_EPSILON);
+  self->tmpls = calloc(self->cap = _DP_TMPL_INC, sizeof(dp_template_t));
   return self;
+}
+
+void dp_add_template(dp_context_t* self, stroke_t* strk, const char* name) {
+  // Ensure we have enough space.
+  if (self->num >= self->cap) {
+    self->cap += _DP_TMPL_INC;
+    self->tmpls = realloc(self->tmpls, self->cap * sizeof(dp_template_t));
+  }
+
+  _normalize(strk, self->n);
+
+  // Add this template.
+  dp_template_t* next = &self->tmpls[self->num++];
+  next->strk = strk;
+  strncpy(next->name, name, DP_MAX_TMPL_NAME_LEN);
 }
 
 dp_result_t dp_recognize(const dp_context_t* self, stroke_t* strk) {
   // Init for recognition.
-  _normalize(strk, self->norm_size);
+  _normalize(strk, self->n);
 
   // Try each template, see which one works.
-  template_t* tmpl = NULL;
+  dp_template_t* tmpl = NULL;
   double score = DBL_MAX;
-  for (int i = 0; i < self->num_tmpls; i++) {
+  for (int i = 0; i < self->num; i++) {
     double d = _greedy_cloud_match(self, strk, self->tmpls[i].strk);
     if (score > d) {
       score = d;
@@ -158,13 +232,14 @@ dp_result_t dp_recognize(const dp_context_t* self, stroke_t* strk) {
   }
 
   // Normalize score in [0,1] and return the result.
-  dp_result_t result = {
-    tmpl,
-    MAX((2.0 - score) / 2.0, 0)
-  };
+  dp_result_t result = { tmpl, MAX((2.0 - score) / 2.0, 0) };
   return result;
 }
 
 void destroy_dollarp(dp_context_t* self) {
+  for (int i = 0; i < self->num; i++) {
+    stroke_destroy(self->tmpls[i].strk);
+  }
+  free(self->tmpls);
   free(self);
 }
--- src/dollarp/dollarp.h
+++ src/dollarp/dollarp.h
@@ -6,27 +6,79 @@
 #include "common/stroke.h"
 
 //! Maximum allowable name for templates.
-#define MAX_TMPL_NAME_LEN 100
+#define DP_MAX_TMPL_NAME_LEN 100
 
 //! A template -- used to recognize strokes.
 typedef struct {
-  stroke_t* strk;                   //!< The underlying stroke.
-  char name[MAX_TMPL_NAME_LEN+1];   //!< Name of the template.
-} template_t;
+  stroke_t* strk;                     //!< The underlying stroke.
+  char name[DP_MAX_TMPL_NAME_LEN+1];  //!< Name of the template.
+} dp_template_t;
 
-//! The main $P context.
+//! Default value for `dp_context_t.n`.
+#define DP_DEFAULT_N 32
+
+//! Default value for `dp_context_t.epsilon`.
+#define DP_DEFAULT_EPSILON 0.50
+
+/*! The main $P context.
+ *
+ * This structure holds the templates and some heuristic values.  Feel free to
+ * alter `n`, and `epsilon`, but DO NOT mess with `tmpls`, `num`, or `cap`.
+ */
 typedef struct {
-  size_t norm_size;   //!< Number of strokes to use in `normalize`.
-  double epsilon;     //!< \in [0,1], controls # of tested alignments.
+  size_t n;               //!< Number of strokes to use in `normalize`.
+  double epsilon;         //!< \in [0,1], controls # of tested alignments.
+  double step;            //!< Step used for scanning a stroke.
 
-  template_t* tmpls;  //!< Templates to use.
-  size_t num_tmpls;   //!< Number of templates.
+  dp_template_t* tmpls;   //!< Array of templates to use.
+  size_t num;             //!< Number of templates.
+  size_t cap;             //!< Capacity of the `tmpls` array.
 } dp_context_t;
 
 //! A result of calling dp_recognize.
 typedef struct {
-  template_t* tmpl;   //!< The template recognition.
-  int score;          //!< The score of the recognition.
+  dp_template_t* tmpl;    //!< The template recognition.
+  double score;           //!< The score of the recognition.
 } dp_result_t;
+
+
+/*! Creates a new $P context.
+ * \return The context.
+ */
+dp_context_t* dp_context_create();
+
+/*! Sets the epsilon value for the context.
+ *
+ * \param self The context.
+ * \param epsilon The new value.
+ */
+static inline void dp_set_epsilon(dp_context_t* self, double epsilon) {
+  self->epsilon = epsilon;
+  self->step = floor(pow(self->n, 1 - self->epsilon));
+}
+
+/*! Adds a new template to the $P context.  The $P context takes ownership of
+ * the stroke and will most likely mutate it.
+ *
+ * Note that the maximum length of stroke names are limited to
+ * `DP_MAX_TMPL_NAME_LEN`.  Names will be truncated to this length.
+ *
+ * \param strk The stroke the template is based on.
+ * \param name The name of the template.
+ */
+void dp_add_template(dp_context_t* self, stroke_t* strk, const char* name);
+
+/*! Attempts to recognize the stroke as one of the templates previously added to
+ * the context with `db_add_template`.
+ *
+ * \return The result of the recognition; may be `NULL`.
+ */
+dp_result_t dp_recognize(const dp_context_t* self, stroke_t* strk);
+
+/*! Destroys the $P context and frees all its memory
+ *
+ * \param self The $P context to delete.
+ */
+void destroy_dollarp(dp_context_t* self);
 
 #endif
