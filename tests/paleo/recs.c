f7ccfa541f2f8f329877539c84bfd2d13cac30dc
Andrew "Jamoozy" T. C. Sabisch <jamoozy@gmail.com>
2015-11-08 18:35:25 -0800

    Use designators to quiet init warnings.

--- tests/paleo/recs.c
+++ tests/paleo/recs.c
@@ -57,7 +57,13 @@
 
 START_TEST(c_line_result_cpy_possible) {
   // Keep it on the stack!
-  pal_line_sub_result_t sub_res = { 40.09, -2.1, "success", 1 };
+  pal_line_sub_result_t sub_res = {
+    .lse = 40.09,
+    .fa = -2.1,
+    .fmsg = "success",
+    .possible = 1,
+    .line = { 0, NULL }
+  };
   pal_line_result_t res = { &sub_res, 1 };
 
   // Set up memory for test.
